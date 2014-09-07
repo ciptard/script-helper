223924e9d43d3a319c9bf8269745fa8632577b24
cipta.rd <cipta.rd@gmail.com>
2014-09-07 11:52:21 +0000

    changed hero slide text

--- swap-slider.js
+++ swap-slider.js
@@ -1,17 +1,25 @@
-
 // Swap Slider
 
 $(function(){
 
 	if (".hero h1 span") {
 		swapElement = $(".hero h1 span");
-		firstSwap(swapElement);
+		firstSwap1(swapElement);
 	}
 
 });
 
 
 // Welcome page text cycle
+function firstSwap1(swapElement) {	
+	$(swapElement).delay(2000).fadeOut(200, function() {
+		$(this).text("Website Company Profile?").fadeIn(200,
+			function() {
+				firstSwap(swapElement);
+			}
+		);
+	});
+}
 function firstSwap(swapElement) {	
 	$(swapElement).delay(2000).fadeOut(200, function() {
 		$(this).text("Fotografi PreWedding?").fadeIn(200,
@@ -41,9 +49,9 @@ function thirdSwap(swapElement) {
 }
 function forthSwap(swapElement) {	
 	$(swapElement).delay(3000).fadeOut(200, function() {
-		$(this).text("Website Company Profile?").fadeIn(200,
+		$(this).text("Jasa Desain Web Berkualitas").fadeIn(200,
 			function() {
-				firstSwap(swapElement);
+				firstSwap1(swapElement);
 			}
 		);
 	});
